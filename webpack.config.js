e85256f77ef3e4833af29040829812b5ac322f74
huy ngoc pham <huypn@mor.com.vn>
2021-01-26 17:02:35 +0700

    hihi

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+import * as path from 'path'
+import * as webpack from 'webpack'
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+}
